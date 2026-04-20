2505eff6535bac3f12a1d7cf6b3613cfc3eb6dc6
Niah LeBlanc <niahLeblanc@protonmail.com>
2026-04-20 15:22:52 -0500

    change postition

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/Niah-LeBlanc.github.io/', // IMPORTANT
+  base: '/', // IMPORTANT
 })
